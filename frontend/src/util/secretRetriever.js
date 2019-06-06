507b2b19a3fe3d747972621cf6dba30e1fdbf7d9
gesparza3 <gesparza3@mail.csuchico.edu>
2019-06-06 15:20:24 -0700

    Added res.status bug

--- frontend/src/util/secretRetriever.js
+++ frontend/src/util/secretRetriever.js
@@ -3,5 +3,5 @@ const fetch = require("node-fetch");
 export const retrieveSecret = (input) => {
   let headers = {"Content-Type": "application/json"};
   return fetch("http://localhost:8000/?input=" + input, {headers, })
-    .then(res => res.json())
+    .then(res => res.status())
 }
